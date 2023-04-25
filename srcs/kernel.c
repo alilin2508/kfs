1befbcc50dd5afc42437b17b4170d84ebcf94db7
Alice LIN <alilin@e1r10p2.clusters.42paris.fr>
2023-04-25 17:09:56 +0200

    added flags and one more helper

--- srcs/kernel.c
+++ srcs/kernel.c
@@ -49,6 +49,22 @@ size_t strlen(const char* str)
 		len++;
 	return len;
 }
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
  
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
@@ -110,5 +126,7 @@ void kernel_main(void)
 	terminal_initialize();
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("42");
+	// terminal_writestring("42");
+	terminal_putentryat('4', terminal_color, 0, 0);
+	terminal_putentryat('2', terminal_color, 1, 0);
 }
